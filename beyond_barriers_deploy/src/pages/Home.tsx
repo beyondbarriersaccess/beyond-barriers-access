e4b72e0daabf7e21788b4cd3f8208110e9556bb2
beyondbarriersaccess <beyondbarriersaccess@gmail.com>
2026-05-14 18:34:49 -0400

    m

--- beyond_barriers_deploy/src/pages/Home.tsx
+++ beyond_barriers_deploy/src/pages/Home.tsx
@@ -1,23 +1,13 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
-import { Hero } from "@/components/sections/Hero";
-import { About } from "@/components/sections/About";
 import { Services } from "@/components/sections/Services";
-import { Eligibility } from "@/components/sections/Eligibility";
-import { Resources } from "@/components/sections/Resources";
-import { Contact } from "@/components/sections/Contact";
 
-export default function Home() {
+export default function ServicesPage() {
   return (
     <div className="min-h-screen flex flex-col w-full">
       <Navbar />
       <main className="flex-1 w-full">
-        <Hero />
-        <About />
         <Services />
-        <Eligibility />
-        <Resources />
-        <Contact />
       </main>
       <Footer />
     </div>
